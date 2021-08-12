bf5c33c378c76a42680947e4f3faacff057194e4
roger chat <mattrogerpelosi@gmail.com>
2021-08-11 20:01:57 -0400

    commit tree f4c656e79a6ca210937645a45ccc041eae2963e0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(name => name === string || name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(name => name.slice(0,1) === string.slice(0,1)) 
+    } 
+
+
+function matchName(object, string) {
+    return object.filter(driver => driver.name === string);
+}
